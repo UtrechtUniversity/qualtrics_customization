e691f71fefa67a040318886b6d7b179e1623be9e
aristotlekandylas <107751800+AristotleKandylas@users.noreply.github.com>
2023-09-30 21:52:58 +0200

    added code for embedded data in addtional entries

--- Additional_entries/script.js
+++ Additional_entries/script.js
@@ -1,37 +1,64 @@
-$(document).ready(function() {
-    var maxEntries = 2; // Maximum 2 additional entries
+<script>
+    $(document).ready(function() {
+        var maxEntries = 2; // Maximum 2 additional entries
 
-    function updateButtonVisibility() {
-        var entryBlocks = $(".entry_block");
-        var $addEntryButtons = $(".add-entry");
+        function updateButtonVisibility() {
+            var entryBlocks = $(".entry_block");
+            var $addEntryButtons = $(".add-entry");
 
-        // Initially hide all "Remove Entry" buttons
-        $(".remove-entry").hide();
+            // Initially hide all "Remove Entry" buttons
+            $(".remove-entry").hide();
 
-        if (entryBlocks.length < maxEntries + 1) {
-            $addEntryButtons.show();
-        } else {
-            $addEntryButtons.hide();
+            if (entryBlocks.length < maxEntries + 1) {
+                $addEntryButtons.show();
+            } else {
+                $addEntryButtons.hide();
+            }
+
+            // Show "Remove Entry" button in additional blocks
+            entryBlocks.slice(1).find(".remove-entry").show();
         }
 
-        // Show "Remove Entry" button in additional blocks
-        entryBlocks.slice(1).find(".remove-entry").show();
-    }
+        // Function to add an entry block
+        $(document).on("click", ".add-entry", function() {
+            var entryBlock = $(".entry_block:first").clone();
+            var entryCount = $(".entry_block").length + 1;
+            entryBlock.attr("data-entry-id", entryCount); // Set a unique identifier
+            entryBlock.find("input").val("");
+            entryBlock.find(".add-entry").hide();
+            entryBlock.find(".remove-entry").show(); // Show the remove button
+            entryBlock.find("input").attr("id", "fullName_" + entryCount); // Update input ID
+            entryBlock.find("input").attr("name", "fullName_" + entryCount); // Update input name
+            entryBlock.find("input[type='email']").attr("id", "email_" + entryCount); // Update email input ID
+            entryBlock.find("input[type='email']").attr("name", "email_" + entryCount); // Update email input name
+            $("#max-entries-message").before(entryBlock);
+            updateButtonVisibility();
+        });
 
-    // Function to add an entry block
-    $(document).on("click", ".add-entry", function() {
-        // JavaScript code to add an entry block
+        // Function to remove an entry block
+        $(document).on("click", ".remove-entry", function() {
+            if ($(".entry_block").length > 1) {
+                $(this).closest(".entry_block").remove();
+                updateButtonVisibility();
+            }
+        });
+
+        // Function to update embedded data fields
+        $(document).on("input", "input[type='text'], input[type='email']", function() {
+            var inputField = $(this);
+            var entryId = inputField.closest(".entry_block").data("entry-id");
+            var selectedValue = inputField.val();
+
+            var metadataNameField = "metadata_name_" + entryId;
+            var metadataEmailField = "metadata_email_" + entryId;
+
+            if (inputField.attr("id") === "fullName_" + entryId) {
+                Qualtrics.SurveyEngine.setEmbeddedData(metadataNameField, selectedValue);
+            } else if (inputField.attr("id") === "email_" + entryId) {
+                Qualtrics.SurveyEngine.setEmbeddedData(metadataEmailField, selectedValue);
+            }
+        });
+
+        updateButtonVisibility();
     });
-
-    // Function to remove an entry block
-    $(document).on("click", ".remove-entry", function() {
-        // JavaScript code to remove an entry block
-    });
-
-    // Function to update embedded data fields
-    $(document).on("input", "input[type='text'], input[type='email']", function() {
-        // JavaScript code to update embedded data fields
-    });
-
-    updateButtonVisibility();
-});
+</script>
